a54e2c633f7849d888e8498762b6e0c2dacc88de
67070046-Natachai <176611935+67070046-Natachai@users.noreply.github.com>
2024-11-06 00:17:47 +0700

    + เพิ่มสีกราฟ

--- PSCPproject/app.py
+++ PSCPproject/app.py
@@ -13,20 +13,28 @@ def main():
     lis_t = request.form.getlist('cal[]')
     if request.method == "POST":
         try:
-            lis_t = [int(cal) for cal in lis_t if cal.isdigit() and 0 <= int(cal) <= 100]
+            lis_t = [int(cal) for cal in lis_t if cal.isdigit() and 0 <= int(cal) <= 50]
         except ValueError:
-            return "กรุณากรอกคะแนนเป็นตัวเลขในช่วง 0-100", 400
+            return "กรุณากรอกคะแนนเป็นตัวเลขในช่วง 0-50", 400
         list_name = []
         lis_t_f = []
+        colors = []
         for i, score in enumerate(lis_t):
-            if score <= 49:
+            if score < 25:
                 list_name.append(lis_s[i])
                 lis_t_f.append(score)
+                colors.append('darkred')
+            elif 25 <= score <= 35:
+                list_name.append(lis_s[i])
+                lis_t_f.append(score)
+                colors.append('darkorange')
+            else:
+                colors.append('limegreen')
         plt.clf()
         plt.xlabel("Subject")
         plt.ylabel("Total")
-        plt.ylim(0, 100)
-        plt.bar(lis_s, lis_t)
+        plt.ylim(0, 50)
+        plt.bar(lis_s, lis_t, color=colors)
         os.makedirs('static', exist_ok=True)
         plt.savefig('static/new_plot.png')
         return render_template('main.html', plot_url='static/new_plot.png', list_name = list_name)
